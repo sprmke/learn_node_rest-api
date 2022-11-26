a176e3eb03a0100652b176a10b0c2e6301c50c37
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-27 00:34:44 +0800

    377. setting up a post model

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// Libraries
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+// Routes
 const feedRoutes = require('./routes/feed');
+
+dotenv.config();
 
 const app = express();
 
@@ -9,12 +15,22 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
